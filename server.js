a22f30a6629383fccd876230e51111120cc62f72
William King <wpk12345@gmail.com>
2017-11-26 02:32:17 -0500

    Added the logic for orm.js to read, create, and update.  Also created model folder with a burger.js file

--- server.js
+++ server.js
@@ -1,3 +1,11 @@
 var express = require("express");
 var methodOverride = require("method-override");
 var bodyParser = require ("body-parser");
+
+//create an instance of the express app
+var app = express();
+
+//specify the port
+var port = process.env.PORT || 3000;
+
+
