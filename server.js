d8f58e427aafa9824ea682b1eb047ea407ff96cb
William King <wpk12345@gmail.com>
2017-11-26 17:10:25 -0500

    Finished creating all files.  Got server running.

--- server.js
+++ server.js
@@ -8,4 +8,22 @@ var app = express();
 //specify the port
 var port = process.env.PORT || 3000;
 
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Set Handlebars.
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Import routes and give the server access to them.
+var routes = require("./controllers/burgers_controller.js");
+
+app.use("/", routes);
+
+app.listen(port);
+console.log("App listening on port: " + port);
 
